8f84d6c39e1999dcad3e201f0e5141a35de09455
Ken Leidal <ken@poshdev.net>
2016-06-21 13:26:12 -0400

    initial commit

---
+++ EventDispatcher.swift
@@ -0,0 +1,65 @@
+// EventManager.swift
+// Copyright (c) 2016 Posh Development, LLC
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+import Foundation
+
+// Tracks listeners subscribed to an event stream and makes it easy to dispatch events to all subscribed listeners
+class EventDispatcher {
+
+    var serial = false  // If true, the listeners are dispatched serially
+
+    // Subscribe to the events dispatched by the EventManager
+    func startListening(listener: AnyObject) {
+        dispatch_barrier_async(listenersListLockQueue) { [weak self] in
+            self?.listeners.append(listener)
+        }
+    }
+
+    // Unsubscribe to the events dispatched by the EventManager
+    func stopListening(listener: AnyObject) {
+        dispatch_barrier_async(listenersListLockQueue) { [weak self] in
+            if let index = self?.listeners.indexOf({$0 === listener}) {
+                self?.listeners.removeAtIndex(index)
+            }
+        }
+    }
+
+    // Called by the dispatcher to run action on each of the subscribed listeners
+    func forEachListener(action: (AnyObject)->Void) {
+        dispatch_async(listenersListLockQueue) { [weak self] in
+            if self == nil {
+                return
+            }
+            for listener in self!.listeners {
+                self!.scheduleAction(action, forListener: listener)
+            }
+        }
+    }
+
+    private var listeners: [AnyObject] = []
+    private let listenersListLockQueue = dispatch_queue_create("com.poshdevelopment.event_manager_lock", nil)  // Lock when using listeners
+    private let dispatchQueue = dispatch_queue_create("com.poshdevelopment.event_manager_lock", nil)
+
+    private func scheduleAction(action: (AnyObject)->Void, forListener listener: AnyObject) {
+        let block = { () -> Void in
+            action(listener)
+        }
+        if (serial) {
+            dispatch_barrier_async(dispatchQueue, block)
+        } else {
+            dispatch_async(dispatchQueue, block)
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,50 @@
+SwiftEventDispatcher
+====================
+
+SwiftEventDispatcher is a simple Swift class which makes it easy to implement a pub/sub
+event listener paradigm.
+
+For example, one might use it while designing an NSUserDefaults wrapper to subscribe a
+ViewController to settings updates (rather than propogating the update through calls
+from the parent).  For instance:
+
+```swift
+class LocalSettings {
+
+    private let localSettings = NSUserDefaults.standardUserDefaults()
+    static let sharedInstance = LocalSettings()
+
+    // MARK: - initialization
+
+    private let eventDispatcher = EventDispatcher()
+
+    func startListening(listener: SettingsDelegate) {
+        eventDispatcher.startListening(listener)
+    }
+
+    func stopListening(listener: SettingsDelegate) {
+        eventDispatcher.stopListening(listener)
+    }
+
+    var isNewUser: Bool {
+        get {
+            return !localSettings.boolForKey("isNewUser")
+        }
+        set {
+            let oldValue = isNewUser
+            localSettings.setBool(newValue, forKey: "isNewUser")
+            localSettings.synchronize()
+            eventDispatcher.forEachListener({ [weak self] (listener) in
+                if self != nil {
+                    (listener as? SettingsDelegate)?.settingsChanged(self!, didChangeIsNewUser: newValue, oldValue: oldValue)
+                }
+            })
+        }
+    }
+}
+
+protocol SettingsDelegate: class, AnyObject {
+    func settingsChanged(_: LocalSettings, didChangeIsNewUser newValue: Bool, oldValue: Bool)
+}
+```
+
